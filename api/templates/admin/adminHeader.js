69241b93b274ff9cc353d892b86557c007d44fc0
tvwoooooooo <tvwoooooooo@gmail.com>
2026-03-05 02:32:41 +0800

    Delete api directory

--- api/templates/admin/adminHeader.js
+++
@@ -1,9 +0,0 @@
-// templates/admin/adminHeader.js
-export const adminHeaderHTML = `
-<div class="admin-header">
-    <div style="display: flex; align-items: center; gap: 0.5rem;">
-        <div class="back-home-btn" id="backHomeBtn"><i class="fas fa-arrow-left"></i></div>
-        <h1 class="section-title">管理面板</h1>
-    </div>
-</div>
-`;
