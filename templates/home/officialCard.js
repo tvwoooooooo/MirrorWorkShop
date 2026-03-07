42efa34e3df7439050ec785645271579da5b341e
tvwoooooooo <tvwoooooooo@gmail.com>
2026-03-07 14:26:26 +0800

    Delete templates directory

--- templates/home/officialCard.js
+++
@@ -1,7 +0,0 @@
-// templates/home/officialCard.js
-export const officialCardHTML = `
-<div id="officialResultCard" class="official-result-card hide">
-    <span class="official-badge"><i class="fas fa-globe"></i> 官网搜索 · <span id="officialBadgeText">GitHub</span></span>
-    <div id="officialResultsList" class="official-results-list"></div>
-</div>
-`;
